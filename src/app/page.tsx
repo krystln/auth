cc62904b083b04eb95a534610e98f77a46bc2411
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2025-01-14 20:25:46 +0530

    removed login form from /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import UserForm from "@/components/userForm";
 
 export default function Home() {
-	return (
-		<div>
-			Select User you want to login as:
-			<UserForm></UserForm>
-		</div>
-	);
+	return <div>Navigate to links available above</div>;
 }
